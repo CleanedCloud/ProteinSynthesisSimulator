faf0a71eadd78a7f391a7b54bac0f9c6a26d82f0
Jose Juan <josejuanhernandezgalvez@gmail.com>
2023-09-17 16:52:39 +0100

    Nucleic Acid done, with transcript and complementary method created.

--- src/main/java/es/ulpgc/model/NucleicAcid.java
+++ src/main/java/es/ulpgc/model/NucleicAcid.java
@@ -1,13 +1,28 @@
 package es.ulpgc.model;
 
 public enum NucleicAcid {
-    Adenine, Guanine, Cytosine, Thymine, Uracile;
+    Adenine {
+        public NucleicAcid complementary() { return Thymine; }
+        public NucleicAcid transcript() { return Uracil; }
+    },
+    Thymine {
+        public NucleicAcid complementary() { return Adenine; }
+        public NucleicAcid transcript() { return Adenine; }
+    },
+    Guanine {
+        public NucleicAcid complementary() { return Cytosine; }
+        public NucleicAcid transcript() { return Cytosine; }
+    },
+    Cytosine {
+        public NucleicAcid complementary() { return Guanine; }
+        public NucleicAcid transcript() { return Guanine; }
+    },
+    Uracil {
+        public NucleicAcid complementary() { return Thymine; }
+        public NucleicAcid transcript() { return Thymine; }
+    };
 
-    public static NucleicAcid complementary(NucleicAcid nucleicAcid) {
-        return null;
-    }
+    public abstract NucleicAcid complementary();
+    public abstract NucleicAcid transcript();
 
-    public static NucleicAcid transcript(NucleicAcid nucleicAcid) {
-        return null;
-    }
 }
